9b1fd4444bf3bc367358cdbacca27e70382ac8c8
ssw1363 <ssw1363@naver.com>
2020-07-21 09:43:25 +0900

    0721_first

--- js/company.js
+++ js/company.js
@@ -65,14 +65,14 @@ $(document).ready(function(){
     //     }
     // }
 
-    if(window.innerWidth > '991')  {
+    // if(window.innerWidth > '991')  {
       $('.certified-slider').bxSlider({
         mode: 'horizontal',// 가로 방향 수평 슬라이드
         speed: 500,        // 이동 속도를 설정
         pager: false,      // 현재 위치 페이징 표시 여부 설정
         moveSlides: 1,     // 슬라이드 이동시 개수
         slideWidth:260,    // 슬라이드 너비
-        minSlides: 2,      // 최소 노출 개수
+        minSlides: 1,      // 최소 노출 개수
         maxSlides: 4,      // 최대 노출 개수
         slideMargin: 63.5,    // 슬라이드간의 간격
         auto: false,  
@@ -81,8 +81,8 @@ $(document).ready(function(){
         controls: true,    // 이전 다음 버튼 노출 여부
         touchEnabled : (navigator.maxTouchPoints > 0) //크롬에서 링크 안될때 추가
       });
-    }
-else if(window.innerWidth <= '991' ) {
+    // }
+if(window.innerWidth <= '991' ) {
     console.log(window.innerWidth);
   $('.reference-slider').bxSlider({
     mode: 'horizontal',// 가로 방향 수평 슬라이드
@@ -100,19 +100,19 @@ else if(window.innerWidth <= '991' ) {
     touchEnabled : (navigator.maxTouchPoints > 0) //크롬에서 링크 안될때 추가
   });
 
-  $('.certified-slider').bxSlider({
-    mode: 'horizontal',// 가로 방향 수평 슬라이드
-    speed: 500,        // 이동 속도를 설정
-    pager: false,      // 현재 위치 페이징 표시 여부 설정
-    moveSlides: 1,     // 슬라이드 이동시 개수
-    slideWidth:250,    // 슬라이드 너비
-    minSlides: 2,      // 최소 노출 개수
-    maxSlides: 2,      // 최대 노출 개수
-    slideMargin: 5,    // 슬라이드간의 간격
-    auto: false,  
-    autoControls: false,     // 자동 실행 여부
-    // autoHover: true,   // 마우스 호버시 정지 여부
-    controls: true,    // 이전 다음 버튼 노출 여부
-    touchEnabled : (navigator.maxTouchPoints > 0) //크롬에서 링크 안될때 추가
-  });
+  // $('.certified-slider').bxSlider({
+  //   mode: 'horizontal',// 가로 방향 수평 슬라이드
+  //   speed: 500,        // 이동 속도를 설정
+  //   pager: false,      // 현재 위치 페이징 표시 여부 설정
+  //   moveSlides: 1,     // 슬라이드 이동시 개수
+  //   slideWidth:250,    // 슬라이드 너비
+  //   minSlides: 2,      // 최소 노출 개수
+  //   maxSlides: 2,      // 최대 노출 개수
+  //   slideMargin: 10,    // 슬라이드간의 간격
+  //   auto: false,  
+  //   autoControls: false,     // 자동 실행 여부
+  //   // autoHover: true,   // 마우스 호버시 정지 여부
+  //   controls: true,    // 이전 다음 버튼 노출 여부
+  //   touchEnabled : (navigator.maxTouchPoints > 0) //크롬에서 링크 안될때 추가
+  // });
 }
